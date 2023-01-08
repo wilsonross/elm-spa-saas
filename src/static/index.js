9daf241c4464382868d9c9d8d9c9253478d07e10
Ross Wilson <ross.wilson@findross.dev>
2023-01-08 21:29:47 +0000

    Added tailwindcss
    Added tailwindcss and configured using postcss
    webpack loader. Styles are currently loaded in
    JS
    
    * Added autoprefixer
    * Added css-loader
    * Added postcss-loader
    * Added style-loader
    * Added tailwindcss

--- src/static/index.js
+++ src/static/index.js
@@ -1,4 +1,5 @@
 import { Elm } from "../elm/Main";
+import "./style.css";
 
 const node = document.getElementById("main");
 const apiUrl = process.env.APP_API_URL;
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 const path = require("path");
+const autoprefixer = require("autoprefixer");
+const tailwindcss = require("tailwindcss");
+
+const Dotenv = require("dotenv-webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
 
-const debug = process.env.npm_lifecycle_event === "build" ? false : true;
-const mode = debug ? "development" : "production";
+const development = process.env.npm_lifecycle_event === "build" ? false : true;
+const mode = development ? "development" : "production";
 const servePath = path.join(__dirname, "dist");
 const entryPath = path.join(__dirname, "src/static/index.js");
 const outputPath = path.join(__dirname, "dist");
-const outputFilename = !debug ? "[name]-[hash].js" : "[name].js";
+const outputFilename = !development ? "[name]-[hash].js" : "[name].js";
 
 console.log("Webpack mode: " + mode);
 
@@ -36,6 +39,21 @@ module.exports = (env) => {
               pathToElm: "node_modules/.bin/elm",
             },
           },
+        },
+        {
+          test: /\.(sa|sc|c)ss$/i,
+          use: [
+            "style-loader",
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: [tailwindcss, autoprefixer],
+                },
+              },
+            },
+          ],
         },
       ],
     },
