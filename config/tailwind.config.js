f51b771e454b2b2447b1fef02bfd29cd9050b70f
Ross Wilson <ross.wilson@findross.dev>
2023-01-09 13:00:19 +0000

    Added colours from design
    * Added colours from design palette to tailwind
      colour config

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
       sans: ["'Inter'", "sans-serif"],
       heading: ["'Inter'", "sans-serif"],
     },
+    colors: {
+      turq: "#2BCBBA",
+      grey: {
+        0: "#f2f3f6",
+        1: "#2d3748",
+        2: "#778CA3",
+      },
+    },
   },
   plugins: [],
 };
